5f50fb3bf722b95eb2507fc6e5f292f6ee95037f
Fetah .OK <ouldkradda.fateh@gmail.com>
2021-11-16 15:45:28 +0000

    🍞 ready to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@mui/material";
 import { Box } from "@mui/system";
-import { useEffect, useState } from "react";
-import { Button, Typography } from "@mui/material";
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [displayContent, setDisplayContent] = useState("0");
+  const [displayContent, setDisplayContent] = useState<string>("0");
+  const [result, setResult] = useState<Number>(0);
+  const [isEval, setIsEval] = useState<Boolean>(false);
+  const [canBeDecimal, setCanBeDecimal] = useState(true);
 
   const opernads = [
     { id: "zero", key: "0" },
@@ -22,39 +25,47 @@ const App = () => {
   const operator = [
     { id: "add", key: "+" },
     { id: "subtract", key: "-" },
-    { id: "multiply", key: "x" },
+    { id: "multiply", key: "*" },
     { id: "divide", key: "/" },
   ];
-  const absOperators = ["+", "x", "/"];
+  const absOperators = ["+", "*", "/"];
+  const negOperators = ["-"];
+  const allOperators = absOperators.concat(negOperators);
 
   const extraKeys = [
     {
       id: "equals",
       key: "=",
-      method: () => {},
+      method: () => {
+        setDisplayContent(eval(displayContent).toString());
+        setIsEval(true);
+        setResult(eval(displayContent).toString());
+        setCanBeDecimal(true);
+      },
     },
     {
       id: "decimal",
       key: ".",
-      method: () => {},
+      method: () => {
+        if (
+          !allOperators.concat(".").includes(displayContent.substr(-1)) &&
+          canBeDecimal
+        ) {
+          setDisplayContent((prevState) => prevState.concat("."));
+          setCanBeDecimal(false);
+        }
+      },
     },
     {
       id: "clear",
       key: "CE",
       method: () => {
+        setIsEval(false);
+        setCanBeDecimal(true);
         setDisplayContent("0");
       },
     },
   ];
-
-  useEffect(() => {
-    let effect = true;
-    return () => {
-      // eslint-disable-next-line
-      effect = false;
-    };
-  }, []);
-
   return (
     <Box
       className="App"
@@ -75,29 +86,89 @@ const App = () => {
         display="flex"
         flexWrap="wrap"
       >
-        <Box border={1} width="100%" p={1} mb={1} textAlign="right">
-          <Typography id="display"> {displayContent}</Typography>
+        <Box
+          border={1}
+          width="100%"
+          overflow="auto"
+          p={1}
+          mb={1}
+          textAlign="right"
+          display="flex"
+          flexWrap="wrap"
+          id="displayC"
+          fontFamily="Roboto"
+          fontSize={12}
+        >
+          {displayContent}
+        </Box>
+        <Box
+          border={1}
+          width="100%"
+          overflow="auto"
+          p={1}
+          mb={1}
+          textAlign="right"
+          display="flex"
+          flexWrap="wrap"
+          id="display"
+          fontFamily="Roboto"
+          fontSize={12}
+        >
+          {isEval ? result : displayContent}
         </Box>
 
         {extraKeys.map((item) => (
-          <Box flex={1}>
+          <Box flex={1} key={item.id}>
             <Button variant="contained" id={item.id} onClick={item.method}>
               {item.key}
             </Button>
           </Box>
         ))}
         {operator.map((item) => (
-          <Box flex={1}>
+          <Box flex={1} key={item.id}>
             <Button
               variant="contained"
               id={item.id}
               onClick={() => {
-                if (displayContent.substr(-1) === item.key) {
+                setCanBeDecimal(true);
+                const last2Chars = displayContent.substr(-2);
+                const isLast2CharsNotOperators =
+                  !allOperators.includes(last2Chars[0]) ||
+                  !allOperators.includes(last2Chars[1]);
+
+                if (!isLast2CharsNotOperators) {
                   setDisplayContent((prevState) =>
-                    prevState.replace(/.$/, item.key)
+                    prevState.replace(/.{0,2}$/, item.key)
                   );
-                } else if (displayContent.substr(-1) !== "-") {
-                  setDisplayContent((prevState) => prevState.concat(item.key));
+                }
+
+                if (isLast2CharsNotOperators) {
+                  if (
+                    absOperators.includes(displayContent.substr(-1)) &&
+                    absOperators.includes(item.key)
+                  ) {
+                    setDisplayContent((prevState) =>
+                      prevState.replace(/.$/, item.key)
+                    );
+                  } else if (
+                    allOperators.includes(displayContent.substr(-1)) &&
+                    negOperators.includes(item.key)
+                  ) {
+                    setDisplayContent((prevState) =>
+                      prevState.concat(item.key)
+                    );
+                  } else if (
+                    absOperators.includes(item.key) &&
+                    negOperators.includes(displayContent.substr(-1))
+                  ) {
+                    setDisplayContent((prevState) =>
+                      prevState.replace(/.$/, item.key)
+                    );
+                  } else {
+                    setDisplayContent((prevState) =>
+                      prevState.concat(item.key)
+                    );
+                  }
                 }
               }}
             >
@@ -106,12 +177,23 @@ const App = () => {
           </Box>
         ))}
         {opernads.map((item) => (
-          <Box flex={1}>
+          <Box flex={1} key={item.id}>
             <Button
               variant="contained"
               id={item.id}
               style={{ width: "100%" }}
               onClick={() => {
+                const lastTwoChars = displayContent.substr(-2);
+                if (
+                  lastTwoChars.length === 2 &&
+                  negOperators.includes(lastTwoChars[0]) &&
+                  negOperators.includes(lastTwoChars[1])
+                ) {
+                  setDisplayContent((prevState) =>
+                    prevState.replace(/.{0,2}$/, "+")
+                  );
+                }
+
                 if (item.key === "0" && displayContent === "0") {
                 } else if (displayContent === "0") {
                   setDisplayContent(item.key);
